dd5acbb623c22af373c73f28e2a5d94c46c70f34
Vukile Simelane <vukile@esoftwaresolutions.co.za>
2021-03-19 14:56:46 +0200

    used tab view fpr challenges page and added tabs on profile  page

---
+++ src/FitnessApp/Converters/SelectedIndexColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FitnessApp.Converters
+{
+    public class SelectedIndexColorConverter : IValueConverter
+    {
+        Color selectedColor;
+        Color unSelectedColor;
+        public SelectedIndexColorConverter()
+        {
+            selectedColor = (Color)Application.Current.Resources["Accent"];
+            unSelectedColor = (Color)Application.Current.Resources["TextSecondary"];
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int.TryParse(parameter as string, out int index);
+            return (int)value == index ? selectedColor : unSelectedColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FitnessApp/ViewModels/ChallengesViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessApp.ViewModels
+{
+    public class ChallengesViewModel : BaseViewModel
+    {
+        public ChallengesViewModel()
+        {
+            SelectCategoryCommand = new Command<string>(OnSelectCategory);
+        }
+
+
+        int selectedCategoryIndex;
+        public int SelectedCategoryIndex 
+        {
+            get => selectedCategoryIndex;
+            set => SetProperty(ref selectedCategoryIndex, value);
+        }
+
+        public Command<string> SelectCategoryCommand { get; }
+
+        void OnSelectCategory(string index)
+        {
+            if (int.TryParse(index, out int i))
+            {
+                SelectedCategoryIndex = i;
+            }
+        }
+
+    }
+}
